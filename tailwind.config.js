81835d99a6ff7d861a887f84b001f0dc3ef13f05
peter youssef <PeterYousse162003@gmail.com>
2024-07-23 17:53:48 +0300

    first commit to git

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".//*.{html,js}"],
+  theme: {
+    colors: {
+      'white' : '#fff',
+      'dark' : '#202C33',
+      'navColor' : '#58707A',
+      'trqLight' : '#26A68E'
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
